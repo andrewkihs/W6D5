0a08dc6caa5ab00fd1ef044186e87894739e2930
Andrew Kihs <ajkihs@gmail.com>
2021-04-02 15:54:49 -0400

    Finish age method

--- ninety_nine_cats/app/models/cat.rb
+++ ninety_nine_cats/app/models/cat.rb
@@ -6,5 +6,12 @@ class Cat < ApplicationRecord
   validates :cats, :sex, presence: true
   validates :cats, :description, presence: true
 
-
+  def age(birth_date)
+    birth_arr = birth_date.split("/") #[yr, month, day]
+    yr = birth_arr.first
+    month = birth_arr[1]
+    current_time = (Time.now.year + Time.now.month/12.0)
+    birth_time = yr + month/12.0
+    (current_time - birth_time).round(1)
+  end
 end
--- ninety_nine_cats/db/seeds.rb
+++ ninety_nine_cats/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.delete_all
+cat_1 = Cat.create(
+    name:'Pans', 
+    birth_date: '2007/04/01', 
+    color: 'Calico', 
+    sex: 'F', 
+    description: "Tim's cat!"
+)
+cat_2 = Cat.create(
+    name:'Salem', 
+    birth_date: '1960/01/20', 
+    color: 'Black', 
+    sex: 'M', 
+    description: "Sabrina the teenage witch's talking cat"
+)
+cat_3 = Cat.create(
+    name:'Grumpy Cat',
+    birth_date: '2006/04/20', 
+    color: 'White', 
+    sex: 'M', 
+    description: "Grumpy cat from the meme"
+)
