74492ce9cf55da3dbc226ff0003e476f631bbdfb
Tim Fraczak <tfraczak@gmail.com>
2021-04-02 16:41:06 -0400

    created age class method for Cat model, and trying to figure out index view

--- ninety_nine_cats/app/models/cat.rb
+++ ninety_nine_cats/app/models/cat.rb
@@ -1,17 +1,12 @@
 class Cat < ApplicationRecord
 
-  validates :cats, :birth_date, presence: true
-  validates :cats, :color, presence: true
-  validates :cats, :name, presence: true
-  validates :cats, :sex, presence: true
-  validates :cats, :description, presence: true
+  validates :birth_date, presence: true
+  validates :color, presence: true
+  validates :name, presence: true
+  validates :sex, presence: true
+  validates :description, presence: true
 
-  def age(birth_date)
-    birth_arr = birth_date.split("/") #[yr, month, day]
-    yr = birth_arr.first
-    month = birth_arr[1]
-    current_time = (Time.now.year + Time.now.month/12.0)
-    birth_time = yr + month/12.0
-    (current_time - birth_time).round(1)
+  def self.age(birth_date)
+    ((Time.now.to_date - birth_date)/365.25).round(1)
   end
 end
--- ninety_nine_cats/db/seeds.rb
+++ ninety_nine_cats/db/seeds.rb
@@ -6,25 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Cat.delete_all
-cat_1 = Cat.create(
+# Cat.destroy_all
+cat_1 = Cat.new(
     name:'Pans', 
     birth_date: '2007/04/01', 
     color: 'Calico', 
     sex: 'F', 
     description: "Tim's cat!"
-)
-cat_2 = Cat.create(
+).save
+cat_2 = Cat.new(
     name:'Salem', 
     birth_date: '1960/01/20', 
     color: 'Black', 
     sex: 'M', 
     description: "Sabrina the teenage witch's talking cat"
-)
-cat_3 = Cat.create(
+).save
+cat_3 = Cat.new(
     name:'Grumpy Cat',
     birth_date: '2006/04/20', 
     color: 'White', 
     sex: 'M', 
     description: "Grumpy cat from the meme"
-)
+).save
