c62811f2cb325cadb4c27888184657c60334c2d9
Tim Fraczak <tfraczak@gmail.com>
2021-04-02 15:26:41 -0400

    did initial routes for cats controller, the views for cats#index, cats#show, and the corresponding actions

---
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -0,0 +1,16 @@
+class CatsController < ApplicationController
+
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end
+
+  # work on the model, create a couple cat seeds
+  # create age method to figure out the sure
+
+end
